96c11d269575ca081d759d50ba7939742f76fd38
Dzananov <danni_daa@hotmail.com>
2022-11-14 09:23:31 +0000

    add login/logout and permission file, added view to post

--- scb/urls.py
+++ scb/urls.py
@@ -1,4 +1,4 @@
-"""drf_api URL Configuration
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -17,5 +17,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
+    path('', include('post.urls')),
 ]
