ac8183b90480df707ca9a8daa9e216f54df6e492
Dzananov <danni_daa@hotmail.com>
2022-11-15 22:46:14 +0000

    add root to startpage, started to connect to heroku

--- scb/urls.py
+++ scb/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,5 @@ urlpatterns = [
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
     path('', include('post.urls')),
+    path('', root_route),
 ]
