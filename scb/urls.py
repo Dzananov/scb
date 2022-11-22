d34150065512a38d11fb999648f8adda55709cc0
Dzananov <danni_daa@hotmail.com>
2022-11-22 11:03:53 +0000

    added comments path url to main url

--- scb/urls.py
+++ scb/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
     path('', include('post.urls')),
+    path('', include('comments.urls')),
     path('', root_route),
 ]
