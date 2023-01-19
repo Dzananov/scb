21de8a4c62371b7190e1513806af1675097c7501
Dzananov <danni_daa@hotmail.com>
2023-01-19 15:40:10 +0000

    install all packages, add to settings

--- scb/settings.py
+++ scb/settings.py
@@ -143,11 +143,11 @@ if 'DEV' in os.environ:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+     }
 else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
 print('connected')
 
 
