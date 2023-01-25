bc69a45d7080b910f311ab3e2b20909cb93d5e45
Dzananov <danni_daa@hotmail.com>
2023-01-25 21:50:30 +0000

    make change in default image profile

--- scb/settings.py
+++ scb/settings.py
@@ -33,7 +33,8 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS':  
+    'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%a, %d %b %Y %H:%M:%S'
 }
